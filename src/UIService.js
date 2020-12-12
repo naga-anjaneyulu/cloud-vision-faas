ba189b1a86ce2ca5076d80b8207021f16a9a631b
Naga Anjaneyulu <nakopa@iu.edu>
2020-12-11 19:38:47 -0500

    Changes

--- src/UIService.js
+++ src/UIService.js
@@ -6,7 +6,7 @@ class UIService{
 
     submitUrl(data){
 
-       return axios.post('http://localhost:8080/',{  headers : {
+       return axios.post('https://us-central1-naga-kopalle2.cloudfunctions.net/vision-faas',{  headers : {
         'Content-Type': 'application/json'
     },
        data})
